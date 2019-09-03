59fd10cf59c72c797d3ae68a3abb517685ef1151
Daniel Lechok <dclechok@gmail.com>
2019-09-03 16:07:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,9 +6,11 @@ def find_element_index(array, value_to_find)
     if(array[i] == value_to_find)
       return i
     end
-    return NIL
+		#i += 1
   end
+	#return nil
 end
+
 
 =begin
 def find_max_value(array)
