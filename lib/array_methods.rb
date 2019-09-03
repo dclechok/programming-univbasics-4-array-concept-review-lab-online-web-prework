3b9b69c295cf7fa7e5363170b33d1c1c67695335
Daniel Lechok <dclechok@gmail.com>
2019-09-03 16:15:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,8 +15,8 @@ def find_max_value(array)
     if(array[i] > highest_int)
       highest_int = array[i]
     end
-    return highest_int
   end
+  return highest_int
 end
 
 def find_min_value(array)
